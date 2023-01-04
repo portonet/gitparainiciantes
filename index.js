0eb4aa35c68e17b9b3b117b61876dea111c16857
Aline <43306767+portonet@users.noreply.github.com>
2023-01-04 19:33:52 -0300

    nova rota adicionada
    nova rota adicionada

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.status(200).send({message: "olha eu aqui no outro repositorio"});
 })
 
+app.get('/desktop', (req, res) => {
+  res.status(200).send({message: "validando no github desktop"});
+})
+
 app.listen(4001, ()=>{
   console.log('Api inicializada na porta 4001');
 })
